165d7c0b0a7c7a07df00e3fba2ff4105e1545542
trevor-crowley <trevor_crowley@hotmail.com>
2017-11-13 17:40:17 -0500

    migrate legacy

---
+++ redmine/design/20170426-tc-remine-rest.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[6]:
+
+get_ipython().system('pip install python-redmine')
+
+
+# In[7]:
+
+from redminelib import Redmine
+
+
+# In[12]:
+
+help (Redmine)
+
+
+# In[ ]:
+
+
+
